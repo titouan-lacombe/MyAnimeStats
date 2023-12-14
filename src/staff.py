defb71fb9f8094877f082194dddea547f9196a9c
Titouan Lacombe <titouan.lacombe99@gmail.com>
2023-12-14 20:11:19 +0100

    better staff data

--- src/staff.py
+++ src/staff.py
@@ -11,7 +11,7 @@ async def get_staff(animes, score_min=0, position_blacklist=[]):
 	# Filter animes by status and score
 	def filter_anime(anime):
 		return anime["my_status"] != "Plan to Watch" and anime["my_score"] >= score_min
-	animes = {anime["mal_id"]: anime for anime in animes if filter_anime(anime)}
+	animes = [anime for anime in animes if filter_anime(anime)]
 
 	persons = {}
 	def register_person(person):
@@ -23,33 +23,35 @@ async def get_staff(animes, score_min=0, position_blacklist=[]):
 		person['characters'] = {}
 		return person
 
-	def add_positions(person, anime_id, positions):
+	def add_positions(person, anime, positions):
 		person = register_person(person)
-		person['animes'][anime_id] = {
-			"anime": animes[anime_id],
+		person['animes'][anime['mal_id']] = {
+			"anime": anime,
 			"positions": positions
 		}
 
-	def add_character(person, anime_id, character):
+	def add_character(person, anime, character):
 		person = register_person(person)
-		# TODO add anime list to keep track of which anime the character is from and sort by date
+		if character['mal_id'] in person['characters']:
+			person['characters'][character['mal_id']]['animes'].append(anime)
+			return
 		person['characters'][character['mal_id']] = {
-			"anime": animes[anime_id],
+			"animes": [anime],
 			"character": character
 		}
 
-	for anime in tqdm(animes.values()):
+	for anime in tqdm(animes):
 		staff = await staff_cache.get(anime=anime)
 		for staff_member in staff:
 			positions = [position for position in staff_member['positions'] if position not in position_blacklist]
-			add_positions(staff_member['person'], anime['mal_id'], positions)
+			add_positions(staff_member['person'], anime, positions)
 
 		characters = await character_cache.get(anime=anime)
 		for character in characters:
 			for voice_actor in character['voice_actors']:
 				if voice_actor['language'] != 'Japanese':
 					continue
-				add_character(voice_actor['person'], anime['mal_id'], character['character'])
+				add_character(voice_actor['person'], anime, character['character'])
 
 	# Convert dict to list and sort
 	persons = list(persons.values())
@@ -57,8 +59,13 @@ async def get_staff(animes, score_min=0, position_blacklist=[]):
 		# Sort anime by airing date (to reflect artist evolution)
 		person['animes'] = list(person['animes'].values())
 		person['animes'].sort(key=lambda anime: anime['anime']['aired']['from'])
+
+		# Sort characters by anime airing date (to reflect artist evolution)
 		person['characters'] = list(person['characters'].values())
-		person['characters'].sort(key=lambda character: character['anime']['aired']['from'])
+		for character in person['characters']:
+			character['animes'].sort(key=lambda anime: anime['aired']['from'])
+		person['characters'].sort(key=lambda character: character['animes'][0]['aired']['from'])
+		
 		person['works'] = len(person['animes']) + len(person['characters'])
 
 	log.info(f"Found {len(persons)} persons")
@@ -81,13 +88,13 @@ def print_staff(persons, show_top=10, min_works=2):
 		for work in person['animes']:
 			anime = work["anime"]
 			anime_title = anime['title_english'] or anime['title']
-			print(f"[{anime['aired']['from'].year}] {anime_title}: {', '.join(work['positions'])}")
+			print(f"[{anime['aired']['from'].year}] {', '.join(work['positions'])} ({anime_title})")
 
 		if len(person['characters']) > 0:
 			print("Character voice actor:")
 		for work in person['characters']:
 			character = work["character"]
-			anime = work["anime"]
-			anime_title = anime['title_english'] or anime['title']
-			print(f"[{anime['aired']['from'].year}] {anime_title}: {character['name']} VA")
+			animes = work["animes"]
+			animes_title = ', '.join([anime['title_english'] if anime['title_english'] is not None else anime['title'] for anime in animes])
+			print(f"[{animes[0]['aired']['from'].year}] {character['name']} ({animes_title})")
 	
