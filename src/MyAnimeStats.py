14d452a2675c2a2c8524c32f5eb5ff762247e2a4
Titouan Lacombe <titouan.lacombe99@gmail.com>
2024-06-23 21:33:09 +0200

    better home

--- src/MyAnimeStats.py
+++ src/MyAnimeStats.py
@@ -6,7 +6,6 @@ set_page_config()
 st.image("src/static/logo.png", width=300)
 
 st.write("Welcome to MyAnimeStats!")
-
 st.write("Here you will be able to analyse your MyAnimeList statistics and get insights on your watching habits.")
-
-st.write("To get started, head to the [List Analysis](Analyse) page.")
+st.write("To get started, click on the link below:")
+st.page_link("pages/1_Analyse.py")
