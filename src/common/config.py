e5139041011f13876cc613dc311fc1f68ae819fc
Titouan Lacombe <titouan.lacombe99@gmail.com>
2025-03-20 23:21:30 +0100

    using pydantic settings

--- src/common/config.py
+++ src/common/config.py
@@ -1,27 +1,8 @@
-from os import getenv
-
-from pydantic import BaseModel
+from pydantic_settings import BaseSettings
 
 
-class Config(BaseModel):
-    @classmethod
-    def from_env(cls, **overrides):
-        build_args = {}
-
-        for key in cls.model_fields.keys():
-            if key in overrides:
-                build_args[key] = overrides[key]
-                continue
-
-            env_val = getenv(key.upper())
-            if env_val is not None:
-                build_args[key] = env_val
-
-        return cls(**build_args)
-
-
-class AppConfig(Config):
+class AppConfig(BaseSettings):
     allow_import: bool = False
 
 
-config = AppConfig.from_env()
+config = AppConfig()
